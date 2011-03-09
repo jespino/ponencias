354c3c4862e099007fb7f8064790c8aabea0142d
Jesús Espino <jespinog@gmail.com>
2011-03-09 22:06:28 +0100

    Añadido al ejemplo una confirmación

--- 2011/python-esp-centro/fabric/ejemplos/script/fabfile.py
+++ 2011/python-esp-centro/fabric/ejemplos/script/fabfile.py
@@ -1,5 +1,6 @@
 from fabric.contrib.files import append
+from fabric.contrib.console import confirm
 
 def add_alias(alias,destino):
-    #append("/etc/aliases", alias+": "+destino, True)
-    append(alias+": "+destino, "/etc/aliases", True)
+    if confirm("Esta seguro de que desea crear el alias: ("+alias+": "+destino+") ?"):
+        append(alias+": "+destino, "/etc/aliases", True)
